ff7a434dcc7534ea08a2cad29ab3d0d1c6ba3b17
Kousigan <kousiganmoni@gmail.com>
2025-11-17 18:13:55 +0530

    first-commit

---
+++ rollup.config.js
@@ -0,0 +1,54 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+
+export default [
+  {
+    input: 'src/index.js',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named',
+      },
+      {
+        file: 'dist/index.esm.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.umd.js',
+        format: 'umd',
+        sourcemap: true,
+        name: 'MuiCascade',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@mui/material': 'MUI',
+          '@mui/icons-material': 'MUIicons',
+          '@emotion/react': 'EmotionReact',
+          '@emotion/styled': 'EmotionStyled',
+          '@mui/system': 'MUIsystem',
+        },
+      },
+    ],
+    external: [],
+    plugins: [
+      peerDepsExternal(),
+      resolve({
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      }),
+      commonjs(),
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: 'dist',
+        rootDir: 'src',
+      }),
+      terser(),
+    ],
+  },
+];
